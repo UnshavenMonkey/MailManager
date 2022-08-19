65e2c88d790ad103eb4dff82cc239062b541bb12
UnshavenMonkey <dudkov_aleksandr@mail.ru>
2022-08-19 12:45:22 +0300

    start project

---
+++ server/index.js
@@ -0,0 +1,25 @@
+require('dotenv').config();
+const express = require('express');
+const sequelize = require('./db');
+const cors = require('cors');
+const models = require('./models/models');
+
+
+const PORT = process.env.PORT || 5000;
+
+const app = express()
+app.use(cors());
+app.use(express.json());
+
+const start = async () => {
+    try {
+        await sequelize.authenticate();
+        await sequelize.sync();
+        app.listen(PORT, () => console.log(`server started on port ${PORT}`));
+    } catch(error) {
+        console.log(error);
+    }
+}
+
+start()
+
---
+++ server/models/models.js
@@ -0,0 +1,38 @@
+const sequelize = require('../db');
+const {DataTypes} = require('sequelize');
+
+const User = sequelize.define('user', {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    email: {type: DataTypes.STRING, unique: true},
+    password: {type: DataTypes.STRING},
+    role: {type: DataTypes.STRING, defaultValue: 'USER'},
+    firstname: {type: DataTypes.STRING},
+    lastname: {type: DataTypes.STRING},
+});
+
+const Citizen = sequelize.define('citizen', {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    email: {type: DataTypes.STRING},
+    phone: {type: DataTypes.INTEGER},
+    street: {type: DataTypes.STRING},
+    build: {type: DataTypes.STRING},
+    apartment: {type: DataTypes.INTEGER},
+    firstname: {type: DataTypes.STRING},
+    lastname: {type: DataTypes.STRING},
+})
+
+const Mail = sequelize.define('mail', {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    dateComplete: {type: DataTypes.DATE},
+    author: {type: DataTypes.STRING},
+    address: {type: DataTypes.STRING},
+    file: {type: DataTypes.STRING},
+    responsiblePerson: {type: DataTypes.STRING},
+})
+
+Citizen.hasMany(Mail);
+Mail.belongsTo(Citizen);
+
+module.exports = {
+    User, Citizen, Mail
+}
